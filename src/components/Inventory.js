8d33eb29463998291bc5e4c4287131d96b0fc446
Reka Palanki <reka.palanki@gmail.com>
2020-10-23 13:45:49 +0100

    Creating application layout

---
+++ src/components/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Inventory extends React.Component {
+  render() {
+    return (
+      <div>Hello!</div>
+    )
+  }
+}
+
+export default Inventory;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
+import './css/style.css';
 import StorePicker from './components/StorePicker';
+import App from './components/App';
 
-render(<StorePicker />, document.getElementById('main'));
+render(<App />, document.getElementById('main'));
