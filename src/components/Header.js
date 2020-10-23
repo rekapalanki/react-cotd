37b5166602863b8d9e66f0e48d410fea0ec4c63d
Reka Palanki <reka.palanki@gmail.com>
2020-10-23 14:15:26 +0100

    Converting Header to a stateless arrow function with implicit return keyword

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,22 @@
 import React from 'react';
 
-class Header extends React.Component {
+const Header = ({tagline, age}) => (
+  <header className="top">
+    <h1>
+      Catch
+      <span className="ofThe">
+        <span className="of">Of</span>
+        <span className="the">The</span>
+      </span>
+      Day
+    </h1>
+    <h3 className="tagline">
+      <span>{tagline} {age}</span>
+    </h3>
+  </header>
+)
+
+/* Header extends React.Component {
   render() {
     return (
       <header className="top">
@@ -16,9 +32,8 @@ class Header extends React.Component {
           <span>{this.props.tagline}</span>
         </h3>
       </header>
-
     )
   }
-}
+}*/
 
 export default Header;
