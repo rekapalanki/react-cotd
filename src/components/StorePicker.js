b0908f1e4174c92dda1ceb56f1a168530b99841b
Reka Palanki <reka.palanki@gmail.com>
2020-10-24 15:00:15 +0100

    Adding goToStore event handler and myInput for referencing input field

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,28 @@ import React from 'react';
 import { getFunName } from '../helpers.js'
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. Get the text from that input
+    console.log(this.myInput);
+    // 3. Change the page to /store/input
+  }
+  
   render() {
     console.log(this);
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Plese Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store ➜</button>
       </form>
     )
