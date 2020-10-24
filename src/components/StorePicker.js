2f80b2ddc8494e7491497109251bca72d2575b56
Reka Palanki <reka.palanki@gmail.com>
2020-10-24 15:24:28 +0100

    routing to this.myInput.current.value storeName inside goToStore event handler method to avoid reloading the page

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,14 +4,20 @@ import { getFunName } from '../helpers.js'
 class StorePicker extends React.Component {
   myInput = React.createRef();
 
-  goToStore = (event) => {
+  goToStore = event => {
     // 1. Stop the form from submitting
     event.preventDefault();
     // 2. Get the text from that input
-    console.log(this.myInput);
+    const storeName = this.myInput.value;
     // 3. Change the page to /store/input
+    // StorePicker is a child of Router.
+    // If you investigate the StorePicker props there is a history.push()
+    //  method we want to access.
+    // This results to faster loading because we're not reloading the page,
+    // just tell Router to load another component instead of StorePicker.
+    this.props.history.push(`/store/${storeName}`)
   }
-  
+
   render() {
     console.log(this);
     return (
