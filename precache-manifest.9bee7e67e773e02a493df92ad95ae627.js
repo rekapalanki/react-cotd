dc7dab50faa1f58e5c0a247f0c28c5f07a4fb14b
Reka Palanki <reka.palanki@gmail.com>
2022-08-25 14:02:24 +0100

    Updates

---
+++ precache-manifest.9bee7e67e773e02a493df92ad95ae627.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6fe7985e87d972293a96e8457c4934d",
+    "url": "/react-cotd/index.html"
+  },
+  {
+    "revision": "03ac8026bfc2c9a77ceb",
+    "url": "/react-cotd/static/css/main.b4a895fb.chunk.css"
+  },
+  {
+    "revision": "0756d09266d08579faa0",
+    "url": "/react-cotd/static/js/2.9f966f46.chunk.js"
+  },
+  {
+    "revision": "0ac0f02cf4dc2dce925fe7684e055e3a",
+    "url": "/react-cotd/static/js/2.9f966f46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ac8026bfc2c9a77ceb",
+    "url": "/react-cotd/static/js/main.41f60713.chunk.js"
+  },
+  {
+    "revision": "eec23655907cbd8dcb5a",
+    "url": "/react-cotd/static/js/runtime-main.5dcdd2d2.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/react-cotd/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/react-cotd/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/react-cotd/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/react-cotd/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "c175fe49ca6c599199a81c6791490344",
+    "url": "/react-cotd/static/media/blanch_caps_inline-webfont.c175fe49.svg"
+  },
+  {
+    "revision": "47e26dee9b053b540478750bdf7a87d5",
+    "url": "/react-cotd/static/media/haymaker-webfont.47e26dee.svg"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/react-cotd/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/react-cotd/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/react-cotd/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
